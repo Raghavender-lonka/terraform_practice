dfff2ac03dd952b909df80cd8ad80d474fe7940c
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-08-11 14:32:10 +0530

    add comments

--- iam_user&group/main.tf
+++ iam_user&group/main.tf
@@ -1,3 +1,5 @@
+# to add iam users to group
+
 resource "aws_iam_group_membership" "team" {
   name = "tf-testing-group-membership"
 
@@ -6,10 +8,14 @@ resource "aws_iam_group_membership" "team" {
   group = aws_iam_group.demo_group.name
 }
 
+# to create user group
+
 resource "aws_iam_group" "demo_group" {
   name = var.group_from_module
   #   path = "/users/"
 }
+
+# to create iam users
 resource "aws_iam_user" "lb" {
   count = length(var.username_from_module)
   name     = var.username_from_module[count.index]
