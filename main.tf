73bee6d485ac64f2d73ca83f8cd73359575064a2
Raghavender Reddy Lonka <96302103+Raghavender-lonka@users.noreply.github.com>
2022-08-26 19:41:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,9 +11,14 @@
       }
     }
 
-    # An example resource that does nothing.
-    resource "null_resource" "example" {
-      triggers = {
-        value = "A example resource that does nothing!"
+
+    resource "aws_instance" "web" {
+      ami           = var.ami_from_module
+      instance_type = "t2.micro"
+
+      tags = {
+        Name = "Demo27_modules"
+        owner = "Raghavender"
+        purpose = "Training Tf"
       }
     }
