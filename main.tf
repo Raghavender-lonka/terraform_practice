a27b100090cec33390a65040d4f9e2fcb11f2189
Raghavender Reddy Lonka <96302103+Raghavender-lonka@users.noreply.github.com>
2022-08-26 19:48:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,17 @@
           name = "raghavenderreddylonka"
         }
       }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-2"
+}
     }
 
 
