d09effc0b1d05d27627b3a4b7f237e8bf2a1826a
Raghavender Reddy Lonka <96302103+Raghavender-lonka@users.noreply.github.com>
2022-08-26 19:53:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,11 +16,6 @@
         }
       }
     }
-    # Configure the AWS Provider
-    provider "aws" {
-      region     = "us-east-2"
-        }
-
 
     resource "aws_instance" "web" {
       ami           = "ami-051dfed8f67f095f5"
